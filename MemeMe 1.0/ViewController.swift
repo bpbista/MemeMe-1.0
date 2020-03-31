269a4bb96ca8be16cf5ab9d797406abb26886e12
BP Bista <bista.coder@gmail.com>
2020-03-31 12:03:39 -0400

    Add Camera and Library Pick Options
    Methods and related outlet connections for buttons in toolbar

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -8,13 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    @IBOutlet weak var scrollview:UIScrollView!
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    setupScrollview()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: Setup Methods
+
+    func setupScrollview(){
+        scrollview.minimumZoomScale = 0.1
+        scrollview.maximumZoomScale = 4.0
+        scrollview.zoomScale = 1.0
+        scrollview.delegate = self as? UIScrollViewDelegate
     }
 
-
 }
+
+
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imagePickerView.image = image
+            imagePickerView.endEditing(true)
+            print("picked")
+            imagePickerView.contentMode = .scaleAspectFill
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+    extension ViewController: UIScrollViewDelegate {
+        func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+            return imagePickerView
+        }
+    }
+    
+
 
